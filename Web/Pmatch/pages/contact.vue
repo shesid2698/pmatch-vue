b635b87d67b88d79c85a92278ab5ff34b9fe89ff
Ethan <ethan@zihai.com.tw>
2024-11-06 10:31:38 +0800

    Update 聯絡我們RWD

--- Web/Pmatch/pages/contact.vue
+++ Web/Pmatch/pages/contact.vue
@@ -8,19 +8,19 @@
             </div>
         </div>
         <div class="flex justify-center">
-            <div>
+            <div class="w-100%">
                 <p class="m-0 mb-3">
                     會員帳號： <span class="color-red">*</span>
                 </p>
                 <input
-                    class="entryCol w-380px rounded-5px h-25px ps-1rem pe-1rem pt-.2rem pb-.2rem font-size-1rem mb-3"
+                    class="entryCol w-100% lg-w-380px rounded-5px h-25px lg-ps-1rem lg-pe-1rem ps-0 pe-0 pt-.2rem pb-.2rem font-size-1rem mb-3"
                     type="text"
                     v-model="accountId"
                 />
                 <p class="m-0 mb-3">
                     遊戲平台： <span class="color-red">*</span>
                 </p>
-                <select v-model="platform" class="selectCol w-100% rounded-5px h-32px ps-1rem pe-1rem pt-.2rem pb-.2rem font-size-1rem mb-3">
+                <select v-model="platform" class="selectCol w-100% rounded-5px h-32px lg-ps-1rem lg-pe-1rem ps-0 pe-0 pt-.2rem pb-.2rem font-size-1rem mb-3">
                     <option value="" hidden>請選擇..</option>
                     <option value="">老子有錢</option>
                 </select>
@@ -28,34 +28,34 @@
                     遊戲暱稱： <span class="color-red">*</span>
                 </p>
                 <input
-                    class="entryCol w-380px rounded-5px h-25px ps-1rem pe-1rem pt-.2rem pb-.2rem font-size-1rem mb-3"
+                    class="entryCol w-100% lg-w-380px rounded-5px h-25px lg-ps-1rem lg-pe-1rem ps-0 pe-0 pt-.2rem pb-.2rem font-size-1rem mb-3"
                     type="text" v-model="nickName"
                 />
                 <p class="m-0 mb-3">
                     聯絡人姓名： <span class="color-red">*</span>
                 </p>
                 <input
-                    class="entryCol w-380px rounded-5px h-25px ps-1rem pe-1rem pt-.2rem pb-.2rem font-size-1rem mb-3"
+                    class="entryCol w-100% lg-w-380px rounded-5px h-25px lg-ps-1rem lg-pe-1rem ps-0 pe-0 pt-.2rem pb-.2rem font-size-1rem mb-3"
                     type="text" v-model="name"
                 />
                 <p class="m-0 mb-3">
                     聯絡人信箱： <span class="color-red">*</span>
                 </p>
                 <input
-                    class="entryCol w-380px rounded-5px h-25px ps-1rem pe-1rem pt-.2rem pb-.2rem font-size-1rem mb-3"
+                    class="entryCol w-100% lg-w-380px rounded-5px h-25px lg-ps-1rem lg-pe-1rem ps-0 pe-0 pt-.2rem pb-.2rem font-size-1rem mb-3"
                     type="text" placeholder="ex: johnny.doe@mail.com"
                     v-model="mail"
                 />
                 <p class="m-0 mb-3">聯絡人電話：</p>
                 <input
-                    class="entryCol w-380px rounded-5px h-25px ps-1rem pe-1rem pt-.2rem pb-.2rem font-size-1rem mb-3"
+                    class="entryCol w-100% lg-w-380px rounded-5px h-25px lg-ps-1rem lg-pe-1rem ps-0 pe-0 pt-.2rem pb-.2rem font-size-1rem mb-3"
                     type="text" placeholder="ex: xxx-xxxxxxxx#ext"
                     v-model="telephone"
                 />
                 <p class="m-0 mb-3">
                     問題類別： <span class="color-red">*</span>
                 </p>
-                <select v-model="questionType" class="selectCol w-100% rounded-5px h-32px ps-1rem pe-1rem pt-.2rem pb-.2rem font-size-1rem mb-3">
+                <select v-model="questionType" class="selectCol w-100% rounded-5px h-32px lg-ps-1rem lg-pe-1rem ps-0 pe-0 pt-.2rem pb-.2rem font-size-1rem mb-3">
                     <option value="">商務洽談</option>
                     <option value="">會員帳號</option>
                     <option value="">其他</option>
@@ -64,13 +64,13 @@
                     問題主題： <span class="color-red">*</span>
                 </p>
                 <input
-                    class="entryCol w-380px rounded-5px h-25px ps-1rem pe-1rem pt-.2rem pb-.2rem font-size-1rem mb-3"
+                    class="entryCol w-100% lg-w-380px rounded-5px h-25px lg-ps-1rem lg-pe-1rem ps-0 pe-0 pt-.2rem pb-.2rem font-size-1rem mb-3"
                     type="text"
                     v-model="questionTheme"
                 />
                 <p class="m-0 mb-3">問題描述：</p>
                 <textarea
-                    class="entryCol w-380px rounded-5px h-150px ps-1rem pe-1rem pt-.2rem pb-.2rem font-size-1rem mb-3"
+                    class="entryCol w-100% lg-w-380px rounded-5px h-150px lg-ps-1rem lg-pe-1rem ps-0 pe-0 pt-.2rem pb-.2rem font-size-1rem mb-3"
                     v-model="questionDescription"
                 />
                 <div>
