435546643d075deff8675c1745854a2114eb349c
shesid2698 <shesid2698@gmail.com>
2024-11-19 15:45:41 +0800

    會員中心選單 / template完成

--- Web/Pmatch/pages/member/center/index.vue
+++ Web/Pmatch/pages/member/center/index.vue
@@ -1,4 +1,27 @@
 <template>
-  <div>會員中心</div>
+    <div class="ccontainer w-90% lg:w-70%">
+        <div class="lg:w-160px">
+            <MemberCenter></MemberCenter>
+        </div>
+        <div class="flex-1 pl-20px">
+            <el-button @click="show = !show">Click Me</el-button>
+            <el-collapse-transition>
+                <div v-show="show"
+                     style="height: 400px">
+                    <div class="transition-box">el-collapse-transition</div>
+                    <div class="transition-box">el-collapse-transition</div>
+                </div>
+            </el-collapse-transition>
+        </div>
+    </div>
 </template>
-<script setup></script>
+<script lang="ts" setup>
+const show = ref(false);
+</script>
+<style scoped>
+.ccontainer {
+    height: fit-content;
+    display: flex;
+    margin: 0 auto;
+}
+</style>
---
+++ Web/Pmatch/pages/member/center/myorder.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="ccontainer w-90% lg:w-70%">
+        <div class="lg:w-160px">
+            <MemberCenter></MemberCenter>
+        </div>
+        <div class="flex-1 pl-20px">
+            我的訂單
+        </div>
+    </div>
+</template>
+<script setup></script>
+<style scoped>
+.ccontainer {
+    height: fit-content;
+    display: flex;
+    margin: 0 auto;
+}
+</style>
