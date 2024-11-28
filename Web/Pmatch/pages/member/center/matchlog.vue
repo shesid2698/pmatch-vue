a070c292ffabc9109243862d30c4bf03acee1ae3
shesid2698 <shesid2698@gmail.com>
2024-11-28 09:15:35 +0800

    會員中心/委託媒合紀錄完成

--- Web/Pmatch/pages/member/center/matchlog.vue
+++ Web/Pmatch/pages/member/center/matchlog.vue
@@ -5,6 +5,19 @@
         </div>
         <div class="flex-1 md:pl-20px">
             <div>
+                <div class="mb-5px flex justify-between">
+                    <div>
+                        搜尋: <input type="search"
+                               v-model="searchStr"
+                               @input="FilterData"
+                               class="box-border p-y-1 p-x-3 text-base flex-1 outline-none rounded-1 border-solid border-1 border-[#ced4da] focus:outline-5 focus:outline-[#c2d9fe] focus:outline-offset-0 focus:border-[#A1C0E3] transition duration-200" />
+                    </div>
+                    <div>顯示筆數:<input type="number"
+                              v-model="pageCount"
+                              @change="ChangePageCount"
+                               class="box-border p-y-1 p-x-3 text-base flex-1 outline-none rounded-1 border-solid border-1 border-[#ced4da] focus:outline-5 focus:outline-[#c2d9fe] focus:outline-offset-0 focus:border-[#A1C0E3] transition duration-200" /></div>
+                </div>
+
                 <el-table :data="tableData"
                           :default-sort="{ prop: 'date', order: 'descending' }"
                           style="width: 100%;"
@@ -40,13 +53,19 @@
                                      sortable
                                      :sort-orders="['ascending','descending']" />
                 </el-table>
+                <el-pagination background
+                               layout="prev, pager, next"
+                               :total="OriTableData.length"
+                               :page-size="pageCount"
+                               @change="ChangePage" />
             </div>
         </div>
     </div>
 </template>
 <script lang="ts" setup>
-import type { TableColumnCtx } from 'element-plus';
-
+const searchStr = ref('');
+const pageCount = ref(10);
+const curPage = ref(1);
 interface User {
     date: String;
     platform: String;
@@ -56,8 +75,7 @@ interface User {
     unit: String;
     entrustStatus: String;
 }
-
-const tableData: User[] = [
+const OriTableData: User[] = [
     {
         date: '2016-05-03',
         platform: '滿貫大亨',
@@ -90,11 +108,125 @@ const tableData: User[] = [
         platform: '滿貫大亨',
         matchMaker: 'et1',
         status: '完成',
-        total: '1,200',
+        total: '1,462',
+        unit: '滿幣',
+        entrustStatus: '委託中'
+    },
+    {
+        date: '2016-05-01',
+        platform: '滿貫大亨',
+        matchMaker: 'et1',
+        status: '完成',
+        total: '1,666',
+        unit: '滿幣',
+        entrustStatus: '委託中'
+    },
+    {
+        date: '2016-05-01',
+        platform: '滿貫大亨',
+        matchMaker: 'et1',
+        status: '完成',
+        total: '1,444',
+        unit: '滿幣',
+        entrustStatus: '委託中'
+    },
+    {
+        date: '2016-05-01',
+        platform: '滿貫大亨',
+        matchMaker: 'et1',
+        status: '完成',
+        total: '1,777',
+        unit: '滿幣',
+        entrustStatus: '委託中'
+    },
+    {
+        date: '2016-05-01',
+        platform: '滿貫大亨',
+        matchMaker: 'et1',
+        status: '完成',
+        total: '1,785',
+        unit: '滿幣',
+        entrustStatus: '委託中'
+    },
+    {
+        date: '2016-05-01',
+        platform: '滿貫大亨',
+        matchMaker: 'et1',
+        status: '完成',
+        total: '1,586',
+        unit: '滿幣',
+        entrustStatus: '委託中'
+    },
+    {
+        date: '2016-05-01',
+        platform: '滿貫大亨',
+        matchMaker: 'et1',
+        status: '完成',
+        total: '1,125',
+        unit: '滿幣',
+        entrustStatus: '委託中'
+    },
+    {
+        date: '2016-05-01',
+        platform: '滿貫大亨',
+        matchMaker: 'et1',
+        status: '完成',
+        total: '1,687',
+        unit: '滿幣',
+        entrustStatus: '委託中'
+    },
+    {
+        date: '2016-05-01',
+        platform: '滿貫大亨',
+        matchMaker: 'et1',
+        status: '完成',
+        total: '1,215',
+        unit: '滿幣',
+        entrustStatus: '委託中'
+    },
+    {
+        date: '2016-05-01',
+        platform: '滿貫大亨',
+        matchMaker: 'et1',
+        status: '完成',
+        total: '1,735',
+        unit: '滿幣',
+        entrustStatus: '委託中'
+    },
+    {
+        date: '2016-05-01',
+        platform: '滿貫大亨',
+        matchMaker: 'et1',
+        status: '完成',
+        total: '1,456',
         unit: '滿幣',
         entrustStatus: '委託中'
     }
 ];
+const tableData = ref(null);
+tableData.value = [...OriTableData.slice(0, pageCount.value)];
+const FilterData = () => {
+    ChangePageCount();
+    if (searchStr.value != '') {
+        tableData.value = tableData.value.filter(item =>
+            Object.values(item).some(
+                value => value.toString().includes(searchStr.value) // 將值轉成字串後進行包含檢查
+            )
+        );
+    }
+};
+/**
+ * 切換分頁事件
+ */
+const ChangePage = page => {
+    curPage.value = page;
+    let startSlice = (page - 1) * pageCount.value;
+    let endSlice = page * pageCount.value;
+    tableData.value = [...OriTableData.slice(startSlice, endSlice)];
+};
+const ChangePageCount = ()=>{
+  ChangePage(curPage.value);
+}
 </script>
 <style scoped>
 .ccontainer {
