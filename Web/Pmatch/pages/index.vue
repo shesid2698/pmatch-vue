f4fc8a9865b8e0528f43388de173976f9ccdb501
Ethan <ethan@zihai.com.tw>
2024-12-05 18:11:02 +0800

    首頁 暫時刻板

--- Web/Pmatch/pages/index.vue
+++ Web/Pmatch/pages/index.vue
@@ -1,6 +1,68 @@
 <template>
     <div>
-        <div class="max-w-1320px m-auto ps-5 pe-5">index</div>
+        <div class="max-w-1320px m-auto ps-5 pe-5">
+            <div class="mb-3">
+                <h1 class="m-0 text-center">最安全的遊戲交易平台</h1>
+            </div>
+            <div class="mb-3">
+                <select>
+                    <option>選擇遊戲...</option>
+                </select>
+            </div>
+            <div class="mb-3">banner</div>
+            <div class="mb-3">
+                <div class="flex">
+                    <div class="w-100">
+                        <div>滿貫大亨圖</div>
+                        <div>
+                            <ul>
+                                <li>滿貫金流</li>
+                                <li>希望之花</li>
+                                <li>發大財金庫</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="w-100">
+                        <div>滿貫大亨圖</div>
+                        <div>
+                            <ul>
+                                <li>滿貫金流</li>
+                                <li>希望之花</li>
+                                <li>發大財金庫</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="w-100">
+                        <div>滿貫大亨圖</div>
+                        <div>
+                            <ul>
+                                <li>滿貫金流</li>
+                                <li>希望之花</li>
+                                <li>發大財金庫</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="w-100">
+                        <div>滿貫大亨圖</div>
+                        <div>
+                            <ul>
+                                <li>滿貫金流</li>
+                                <li>希望之花</li>
+                                <li>發大財金庫</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="w-full bg-#eee ps-5 pe-5">
+            <div class="max-w-1320px m-auto ps-5 pe-5">
+                <div>
+                    <h2 class="m-0 text-center">NEWS</h2>
+                    <h1 class="m-0 text-center">最新消息</h1>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
