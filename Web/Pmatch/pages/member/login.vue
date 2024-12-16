90fd68a1ee0c515e1ef2b559c00ddae3e01af554
shesid2698 <shesid2698@gmail.com>
2024-12-16 16:17:07 +0800

    修正enter進入到註冊頁面問題

--- Web/Pmatch/pages/member/login.vue
+++ Web/Pmatch/pages/member/login.vue
@@ -1,13 +1,12 @@
 <template>
-    <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
-        integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
-        crossorigin="anonymous"
-    />
+    <link rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
+          integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
+          crossorigin="anonymous" />
     <div class="ccontainer md:pt-60px">
         <div class="login-card w-100% md:w-378px">
-            <form action="" method="post">
+            <form action=""
+                  method="post">
                 <!-- 登入Title -->
                 <div class="flex flex-items-center">
                     <div class="w-25px h-20px bg-#1a6db4 mr-8px mt-3px"></div>
@@ -16,71 +15,56 @@
 
                 <div class="mt-15px">
                     <div class="mb-5px">登入帳號</div>
-                    <input
-                        v-model="accountId"
-                        type="text"
-                        required
-                        class="box-border p-y-1.5 p-x-3 text-base w-100% outline-none rounded-1 border-solid border-1 border-[#ced4da] focus:outline-5 focus:outline-[#c2d9fe] focus:outline-offset-0 focus:border-[#A1C0E3] transition duration-200"
-                    />
+                    <input v-model="accountId"
+                           type="text"
+                           required
+
+                           class="box-border p-y-1.5 p-x-3 text-base w-100% outline-none rounded-1 border-solid border-1 border-[#ced4da] focus:outline-5 focus:outline-[#c2d9fe] focus:outline-offset-0 focus:border-[#A1C0E3] transition duration-200" />
                 </div>
 
                 <div class="mt-15px">
                     <div class="mb-5px">密碼</div>
                     <div class="relative">
-                        <input
-                            v-model="password"
-                            type="password"
-                            required
-                            ref="i_password"
-                            class="password box-border p-y-1.5 p-x-3 text-base w-100% outline-none rounded-1 border-solid border-1 border-[#ced4da] focus:outline-5 focus:outline-[#c2d9fe] focus:outline-offset-0 focus:border-[#A1C0E3] transition duration-200"
-                            pattern="(?=.*\d)(?=.*[a-zA-Z])[A-Za-z0-9!@#$%&*]{8,20}"
-                        />
-                        <div
-                            @click="turnInputType"
-                            class="cursor-pointer absolute top-50% transform translate-y-[-45%] left-92%"
-                        >
-                            <i ref="eyes" class="fa-solid fa-eye text-gray"></i>
+                        <input v-model="password"
+                               type="password"
+                               required
+                               ref="i_password"
+                               class="password box-border p-y-1.5 p-x-3 text-base w-100% outline-none rounded-1 border-solid border-1 border-[#ced4da] focus:outline-5 focus:outline-[#c2d9fe] focus:outline-offset-0 focus:border-[#A1C0E3] transition duration-200"
+                               pattern="(?=.*\d)(?=.*[a-zA-Z])[A-Za-z0-9!@#$%&*]{8,20}" />
+                        <div @click="turnInputType"
+                             class="cursor-pointer absolute top-50% transform translate-y-[-45%] left-92%">
+                            <i ref="eyes"
+                               class="fa-solid fa-eye text-gray"></i>
                         </div>
 
-                        <div
-                            class="tips absolute text-14px w-200px h-auto bg-dark left-25% bottom-130% text-white p-10px rounded-1"
-                        >
+                        <div class="tips absolute text-14px w-200px h-auto bg-dark left-25% bottom-130% text-white p-10px rounded-1">
                             密碼長度必須為8~20位,
                             其中必須包含至少一位數字、一位英文，若需有特殊符號僅限於
                             ! @ # $ % & *
                         </div>
-                        <div
-                            class="tips absolute clip-path-custom w-15px h-15px bg-dark left-52% bottom-110%"
-                        ></div>
+                        <div class="tips absolute clip-path-custom w-15px h-15px bg-dark left-52% bottom-110%"></div>
                     </div>
                 </div>
-                <div v-if="!hasToken" class="w-100% mt-15px">
-                    <VueTurnstile
-                        site-key="1x00000000000000000000AA"
-                        size="normal"
-                        @update:model-value="onVerify"
-                    ></VueTurnstile>
+                <div v-if="!hasToken"
+                     class="w-100% mt-15px">
+                    <VueTurnstile site-key="1x00000000000000000000AA"
+                                  size="normal"
+                                  @update:model-value="onVerify"></VueTurnstile>
                 </div>
                 <!-- 登入/註冊 -->
                 <div class="mt-15px">
                     <div class="flex">
                         <div class="flex-1">
-                            <NuxtLink to="/register"
-                                ><button
-                                    class="p-y-1.5 p-x-3 w-100% border-none outline-none text-16px text-white rounded-1 bg-[#1a6db4] hover:opacity-70 transition duration-200 cursor-pointer"
-                                >
+                            <NuxtLink to="/register"><button type="button" class="p-y-1.5 p-x-3 w-100% border-none outline-none text-16px text-white rounded-1 bg-[#1a6db4] hover:opacity-70 transition duration-200 cursor-pointer">
                                     註冊
-                                </button></NuxtLink
-                            >
+                                </button></NuxtLink>
                         </div>
                         <div class="w-10px"></div>
                         <div class="flex-1">
-                            <button
-                                @click="login"
-                                type="button"
-                                :disabled="!loginToken"
-                                class="disabled:opacity-70 p-y-1.5 p-x-3 w-100% border-none outline-none text-16px text-white rounded-1 bg-[#e93470] hover:bg-[#bb2d3b] transition duration-200 cursor-pointer"
-                            >
+                            <button @click="login"
+                                    type="button"
+                                    :disabled="!loginToken"
+                                    class="disabled:opacity-70 p-y-1.5 p-x-3 w-100% border-none outline-none text-16px text-white rounded-1 bg-[#e93470] hover:bg-[#bb2d3b] transition duration-200 cursor-pointer">
                                 登入
                             </button>
                         </div>
@@ -89,11 +73,8 @@
             </form>
 
             <div class="mt-15px text-center">
-                已經註冊?<NuxtLink
-                    to="/member/forgetpwd"
-                    class="text-[#0d6efd] hover:opacity-70 hover:underline text-15px no-underline"
-                    >忘記密碼</NuxtLink
-                >
+                已經註冊?<NuxtLink to="/member/forgetpwd"
+                          class="text-[#0d6efd] hover:opacity-70 hover:underline text-15px no-underline">忘記密碼</NuxtLink>
             </div>
         </div>
     </div>
@@ -103,30 +84,30 @@
 import VueTurnstile from 'vue-turnstile';
 const eyes = ref(null);
 const i_password = ref(null);
-const loginToken = useCookie("loginToken");
+const loginToken = useCookie('loginToken');
 const hasToken = ref(loginToken.value !== undefined);
 // 登入用
-let accountId = ref("");
-let password = ref("");
+let accountId = ref('');
+let password = ref('');
 const memberList = ref({});
 const { $axios } = useNuxtApp();
 const router = useRouter();
 // cookies
-let userNameCookie = useCookie("_PmUserName");
-let tokenCookie = useCookie("_PmToken");
+let userNameCookie = useCookie('_PmUserName');
+let tokenCookie = useCookie('_PmToken');
 
 const turnInputType = () => {
-    if (i_password.value.type === "password") {
-        i_password.value.type = "text";
-        eyes.value.classList.remove("fa-eye");
-        eyes.value.classList.add("fa-eye-slash");
+    if (i_password.value.type === 'password') {
+        i_password.value.type = 'text';
+        eyes.value.classList.remove('fa-eye');
+        eyes.value.classList.add('fa-eye-slash');
     } else {
-        i_password.value.type = "password";
-        eyes.value.classList.remove("fa-eye-slash");
-        eyes.value.classList.add("fa-eye");
+        i_password.value.type = 'password';
+        eyes.value.classList.remove('fa-eye-slash');
+        eyes.value.classList.add('fa-eye');
     }
 };
-const onVerify = (tokenValue) => {
+const onVerify = tokenValue => {
     loginToken.value = tokenValue;
     setTimeout(() => {
         hasToken.value = true;
@@ -137,7 +118,7 @@ const onVerify = (tokenValue) => {
 const login = async () => {
     // 加密密碼
     const encryptedPassword = encrypt(password.value);
-    console.log("encrypted pw: " + encryptedPassword);
+    console.log('encrypted pw: ' + encryptedPassword);
 
     // 等待登入結果
     await Login(encryptedPassword);
@@ -147,14 +128,14 @@ const login = async () => {
 async function Login(encryptedPassword) {
     try {
         const response = await $axios.post(
-            "/api/v1/Pmatch/Logon",
+            '/api/v1/Pmatch/Logon',
             {
                 Account: accountId.value,
                 Password: encryptedPassword, // 使用加密後的密碼
-                IsNormalUser: true,
+                IsNormalUser: true
             },
             {
-                headers: {},
+                headers: {}
             }
         );
 
@@ -162,12 +143,12 @@ async function Login(encryptedPassword) {
             memberList.value = response.data.Data;
             userNameCookie.value = response.data.Data.Name;
             tokenCookie.value = response.data.Data.Token;
-            router.push("/");
+            router.push('/');
         } else {
             alert(`${response.data.Status.Message}`);
         }
     } catch (error) {
-        console.error("請求失敗:", error);
+        console.error('請求失敗:', error);
     }
 }
 
@@ -181,6 +162,18 @@ function encrypt(input) {
     // 4. 結果
     return `e${reversedBase64}is`;
 }
+onMounted(() => {
+    window.addEventListener('keydown', EnterLogin);
+});
+onBeforeUnmount(() => {
+    window.removeEventListener('keydown', EnterLogin);
+});
+function EnterLogin(e) {
+  // e.preventDefault();
+    if (e.key == 'Enter') {
+        login();
+    }
+}
 </script>
 
 <style scoped>
@@ -192,7 +185,7 @@ function encrypt(input) {
     width: 100%;
 }
 .font-sans {
-    font-family: Arial, "Noto Sans TC", sans-serif;
+    font-family: Arial, 'Noto Sans TC', sans-serif;
 }
 .clip-path-custom {
     clip-path: polygon(50% 100%, 0 50%, 100% 50%);
