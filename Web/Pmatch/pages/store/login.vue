dfd6dc8734f424e1fcc2803632061e2791588a13
Otto <disabledman@hotmail.com>
2025-01-10 11:38:55 +0800

    Updated MASTER PUBLISH

--- Web/Pmatch/pages/store/login.vue
+++ Web/Pmatch/pages/store/login.vue
@@ -195,8 +195,8 @@ async function login(event, encryptedPassword) {
                     .replace(/\//g, "_");
 
                 // 跳轉到目標網站
-                const targetUrl = `http://192.168.10.206:6092/l/${base64UrlToken}`;
-                // const targetUrl = `https://acctest.pmatch.com.tw/l/${base64UrlToken}`;
+                //const targetUrl = `http://192.168.10.206:6092/l/${base64UrlToken}`;
+                const targetUrl = `https://acctest.pmatch.com.tw/l/${base64UrlToken}`; // MASTER 留正式環境
                 window.location.href = targetUrl;
             } else {
                 console.error("跳轉失敗");
