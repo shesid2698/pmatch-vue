518a32a5d67a477861b5ca8dbbc3657df81f98b9
shesid2698 <shesid2698@gmail.com>
2025-03-27 16:41:45 +0800

    調整聊天室開啟的視窗

--- Web/Pmatch/pages/findmatch/[id].vue
+++ Web/Pmatch/pages/findmatch/[id].vue
@@ -863,7 +863,7 @@ const OpenChat = async () => {
         window.open(
             `${config.webChatUrl}?token=${chatToken}`,
             '_blank',
-            'toolbar=no,location=no,status=no,resizable=no,width=770,height=677'
+            'toolbar=no,location=no,status=no,resizable=no,width=770,height=695'
         );
     }
 };
