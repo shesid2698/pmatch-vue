869ac7885b0b4e712a504e0135adc19a38ad5212
shesid2698 <shesid2698@gmail.com>
2025-04-08 17:18:25 +0800

    修正註冊加密錯誤

--- Web/Pmatch/pages/register/form.vue
+++ Web/Pmatch/pages/register/form.vue
@@ -530,9 +530,9 @@ const RegisterMember = async password1 => {
             Address: allAddress,
             ContractStores: contractStores.value,
             RefferCode: recommendCode.value,
-            IsPromoteCode: encrypt.decrypt(route.query.IsPromoteCode) === 'true' ? true : false, // 是否為下線經營者
+            IsPromoteCode:route.query.IsPromoteCode && encrypt.decrypt(route.query.IsPromoteCode) === 'true' ? true : false, // 是否為下線經營者
             ThirdPartyPlatform: thirdPartyPlatform.value,
-            PmatchStoreIds: encrypt.decrypt(route.query.PmatchStoreIds) // 商家ID
+            PmatchStoreIds: route.query.PmatchStoreIds?encrypt.decrypt(route.query.PmatchStoreIds):'' // 商家ID
         },
         {
             headers: {
