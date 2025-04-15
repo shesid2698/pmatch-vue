ecf364e019b89a6683b833ed37a16705bf96a1f7
Otto <disabledman@hotmail.com>
2025-04-15 10:49:54 +0800

    Fixed 0005506: PMatch 比值的部分將金豹拿掉

--- Web/Pmatch/components/NewRatio.vue
+++ Web/Pmatch/components/NewRatio.vue
@@ -84,11 +84,6 @@ const matchingList = [
     SendPatch: "134",
     CollectPatch: "150",
   },
-  {
-    GamePlatform: "金豹娛樂城",
-    SendPatch: "130",
-    CollectPatch: "144",
-  },
 ];
 
 const visibleList = ref([]); // 當前顯示的列表
