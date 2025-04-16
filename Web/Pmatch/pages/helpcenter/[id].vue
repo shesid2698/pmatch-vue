9605780aa45abc22434da013b0a15387bcb8c28f
Otto <disabledman@hotmail.com>
2025-04-16 14:00:39 +0800

    Fixed 幫助中心單頁樣式

--- Web/Pmatch/pages/helpcenter/[id].vue
+++ Web/Pmatch/pages/helpcenter/[id].vue
@@ -7,14 +7,20 @@
     <Meta property="og:description" content="Pmatch遊戲道具交易平台 – 線上遊戲安心交易的第一選擇，Pmatch為你嚴選商家，用合約保障你的權益，杜絕詐騙，防護交易安全" />
   </Head>
   <div class="mt-7rem max-w-1110px m-auto lg-ps-0 ps-3 lg-pe-0 pe-3">
-    <!-- 成功獲取資料時 -->
-    <div v-for="(item, index) in newsItem" :key="index">
-      <h2 class="color-#666">{{ item.Title }}</h2>
-      <h4>{{ item.Summary }}</h4>
-      <div class="detail">
-        <div v-html="item.Content"></div>
+      <!-- 成功獲取資料時 -->
+      <div v-for="(item, index) in newsItem" :key="index">
+          <div class="w-100% md:w-70% mr-10px">
+              <div class="text-start content-center flex items-center newsContanier cursor-pointer">
+                  <div class="mr-30px"><img src="/images/icon-Q&A.png" width="100" alt=""></div>
+                  <div class="text-[#4361EE] text-24px">{{ item.Title }}</div>
+              </div>
+              <div class="newsContanier text-center" v-html="item.Content">
+              </div>
+              <div class="w-100% text-center mt-50px sid">
+                  <button class="backBtn" @click="goToHelpCenter">回上層</button>
+              </div>
+          </div>
       </div>
-    </div>
   </div>
 </template>
 
@@ -33,6 +39,11 @@ const newsItem = ref(null);
 const { $axios } = useNuxtApp();
 const jwtStore = useJwtStore();
 const userToken = useCookie("_PmToken");
+
+// 跳轉到 /helpcenter
+const goToHelpCenter = () => {
+    window.location.href = '/helpcenter';
+};
 
 // 取得GetNewsDetail
 async function fetchNewsDetailData(token) {
@@ -76,11 +87,118 @@ onMounted(async () => {
   }
 });
 </script>
-
 <style scoped>
-.detail {
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  padding: 1rem;
-}
+    .helpTitle {
+        background: linear-gradient(to right, rgba(67, 97, 238), rgb(247, 37, 142));
+        background: -webkit-linear-gradient(to right, rgba(67, 97, 238), rgb(247, 37, 142));
+        background-clip: text;
+        -webkit-background-clip: text;
+        color: transparent;
+        width: fit-content;
+        margin: 0 auto;
+        margin-bottom: 15px;
+        font-weight: 600;
+    }
+
+    .newsContanier {
+        padding: 2rem 5px;
+        box-sizing: border-box;
+        width: 100%;
+        position: relative;
+        &::before
+        {
+            content: '';
+            position: absolute;
+            inset: 0;
+            border-width: 0;
+            border-bottom-width: 3px;
+            border-style: solid;
+            border-color: transparent;
+            background-image: linear-gradient(to right, rgba(67, 97, 238), rgba(247, 37, 133));
+            background-origin: border-box;
+            mask-image: linear-gradient(white, white), linear-gradient(white, white);
+            mask-clip: padding-box, border-box;
+            mask-composite: exclude, add;
+        }
+    }
+
+    .dot1 {
+        position: absolute;
+        width: 25%;
+        right: 0;
+        top: 0;
+    }
+
+    .dot2 {
+        position: absolute;
+        width: 25%;
+        left: 0;
+        top: 20%;
+    }
+
+    .dot3 {
+        position: absolute;
+        width: 25%;
+        right: 0;
+        top: 60%;
+        transform: rotate(180deg);
+    }
+
+    .searchBoxContainer {
+        width: 300px;
+        height: 55px;
+        background-color: transparent;
+    }
+
+    .searchBox {
+        background-color: transparent;
+        width: 100%;
+        height: 100%;
+        font-size: 25px;
+        padding-left: 15px;
+        padding-right: 55px;
+        box-sizing: border-box;
+        outline: none;
+        border: none;
+        border-width: 2px;
+        border-style: solid;
+        border-color: transparent;
+        border-radius: 2rem;
+        background-image: linear-gradient(white, white), linear-gradient(to right, rgba(67, 97, 238), rgba(247, 37, 133));
+        background-clip: padding-box, border-box;
+        background-origin: padding-box, border-box;
+    }
+
+    .backBtn {
+        width: 175px;
+        aspect-ratio: 175/65;
+        border-width: 2px;
+        border-style: solid;
+        border-color: transparent;
+        border-radius: 50px;
+        font-size: 18px;
+        background-image: linear-gradient(transparent, transparent), linear-gradient(to right, rgba(67, 97, 238), rgba(247, 37, 133));
+        background-clip: padding-box, border-box;
+        background-origin: padding-box, border-box;
+        color: white;
+        cursor: pointer;
+    }
+
+        .backBtn:hover {
+            background-image: linear-gradient(white, white), linear-gradient(to right, rgba(67, 97, 238), rgba(247, 37, 133));
+            color: rgba(247, 37, 133);
+        }
+
+    @media screen and (max-width: 768px) {
+        .searchBoxContainer {
+            margin-top: 20px;
+            width: 100%;
+        }
+
+        .dot1,
+        .dot2,
+        .dot3 {
+            width: 70%;
+        }
+    }
 </style>
