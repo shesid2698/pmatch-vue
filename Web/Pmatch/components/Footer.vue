64ea4193054470bc6129140bc2397b86ca23b794
Otto <disabledman@hotmail.com>
2025-05-06 13:50:21 +0800

    Updated Version

--- Web/Pmatch/components/Footer.vue
+++ Web/Pmatch/components/Footer.vue
@@ -14,7 +14,7 @@
               PMatch 遊戲道具交易平台
             </p>
             <p class="color-#fff m-0 text-center">
-              版本號 : v6.2.2
+              版本號 : v6.2.3
             </p>
           </div>
         </div>
