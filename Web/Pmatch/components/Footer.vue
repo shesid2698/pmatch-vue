b894d964f78cd783a5ffdf68a8a4c5a7e8a2f1c8
benson <benson@zihai.com.tw>
2025-09-01 13:33:18 +0800

    Updatd: PMatch 版號 v6.4.3

--- Web/Pmatch/components/Footer.vue
+++ Web/Pmatch/components/Footer.vue
@@ -14,7 +14,7 @@
               PMatch 遊戲道具交易平台
             </p>
             <p class="color-#fff m-0 text-center">
-              版本號 : v6.4.2
+              版本號 : v6.4.3
             </p>
           </div>
         </div>
