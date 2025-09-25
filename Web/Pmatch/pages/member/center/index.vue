ef5d4b246d9ae4e955118dc7b6395a57337ed3d9
Parker <hanhan010609@gmail.com>
2025-09-25 08:57:48 +0800

    Fixed 0006110: 會員中心 統一編號隱藏

--- Web/Pmatch/pages/member/center/index.vue
+++ Web/Pmatch/pages/member/center/index.vue
@@ -165,11 +165,11 @@
               </div>
             </div>
 
-            <div class="mt-15px">
+            <!-- <div class="mt-15px">
               <div class="mb-5px text-16px">統一編號</div>
               <input type="text" pattern="[0-9]{8}" v-model="theUser[0].TaxId"
                 class="box-border p-y-1.5 p-x-3 text-base w-100% outline-none rounded-1 border-solid border-1 border-[#ced4da] focus:outline-5 focus:outline-[#c2d9fe] focus:outline-offset-0 focus:border-[#A1C0E3] transition duration-200" />
-            </div>
+            </div> -->
 
             <div class="mt-15px">
               <div class="mb-5px text-[#484646] font-400 text-15px">
