250ab6ad1ec0e389994f7ecf1c44c15716e79795
shesid2698 <shesid2698@gmail.com>
2025-10-16 14:59:45 +0800

    add carture warn、error log

---
+++ Web/Pmatch/plugins/error-handler.ts
@@ -0,0 +1,13 @@
+
+export default defineNuxtPlugin((nuxtApp) => {
+    const route = useRoute();
+    nuxtApp.vueApp.config.warnHandler = (msg, instance, info) => {
+        console.warn('🟡 Vue 警告:', msg,instance,info)
+        console.warn('🟡 警告位置',route.fullPath)
+    }
+    // 捕獲真正的運行時錯誤
+    nuxtApp.vueApp.config.errorHandler = (err, instance, info) => {
+        console.error('🔴 運行時錯誤:', err,instance, info)
+        console.error('🟡 警告位置',route.fullPath)
+    }
+})
