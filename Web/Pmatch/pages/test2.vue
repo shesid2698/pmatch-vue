65c377efe55ffafc211cff08ddc42319a0c921d7
shesid2698 <shesid2698@gmail.com>
2025-10-17 10:13:44 +0800

    nuxt test

--- Web/Pmatch/pages/test2.vue
+++ Web/Pmatch/pages/test2.vue
@@ -1,9 +1,11 @@
 <script setup>
 const url = useRequestURL()
+const count = useState('count',()=>10)
 </script>
 
 <template>
-  <div>
+  <div class="w-70% m-auto mt-3%">
     <h2>pathname:{{ url.pathname }}</h2>
+    <button @click="count++">{{ count }}</button>
   </div>
 </template>
