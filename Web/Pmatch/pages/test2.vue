67fb5210d254dd34242e943490229e483c1f5947
shesid2698 <shesid2698@gmail.com>
2025-10-17 09:38:13 +0800

    nuxt4 test

--- Web/Pmatch/pages/test2.vue
+++ Web/Pmatch/pages/test2.vue
@@ -1,13 +1,9 @@
+<script setup>
+const url = useRequestURL()
+</script>
+
 <template>
   <div>
-    <h1 :class="isTableDevice === true ? 'bg-red' : 'bg-green'">cxz;wdwdl;w'jfdskla</h1>
+    <h2>pathname:{{ url.pathname }}</h2>
   </div>
 </template>
-<script setup>
-const isTableDevice = ref(true);
-onMounted(() => {
-  if ('ontouchstart' in window || navigator.maxTouchPoints > 0) {
-    isTableDevice.value = false;
-  }
-});
-</script>
