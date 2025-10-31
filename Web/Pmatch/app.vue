1d57495460f141945c5f1eab121016d1247e982a
shesid2698 <shesid2698@gmail.com>
2025-10-31 16:02:56 +0800

    Updated noscript

--- Web/Pmatch/app.vue
+++ Web/Pmatch/app.vue
@@ -175,15 +175,8 @@ useHead({
   noscript: [
     {
       key: 'gtm-noscript',
-      innerHTML: `
-        <iframe 
-          src="https://www.googletagmanager.com/ns.html?id=GTM-TBHCXFCD"
-          height="0" 
-          width="0" 
-          style="display:none;visibility:hidden">
-        </iframe>
-      `.trim(),
-      tagPosition: 'bodyOpen'  // 必須！放在 <body> 開頭
+      innerHTML: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TBHCXFCD" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+      tagPosition: 'bodyOpen'
     }
   ]
 })
