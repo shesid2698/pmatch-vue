f657a9b6bca62cbafecc977408f55fc8d157aedc
shesid2698 <shesid2698@gmail.com>
2025-11-03 15:35:10 +0800

    Updated: nuxt.config.ts  -> close experimental.appManifest

--- Web/Pmatch/nuxt.config.ts
+++ Web/Pmatch/nuxt.config.ts
@@ -32,4 +32,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         },
     },
     compatibilityDate: '2025-01-20',
+    experimental: {
+        appManifest: false
+    }
 });
