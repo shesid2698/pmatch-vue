1f8256e58f46d2644375183f9aaa57f706974cfa
shesid2698 <shesid2698@gmail.com>
2025-12-02 16:21:26 +0800

    Updated: app.vue註冊FCM

--- Web/Pmatch/app.vue
+++ Web/Pmatch/app.vue
@@ -140,15 +140,16 @@ onMounted(async () => {
     //     console.error("取得通知權限失敗:", err);
     //   });
     // }
+
+  } catch (error) {
+    console.error("頁面初始化失敗:", error);
+  }finally{
     console.log("準備監聽訊息..");
     await requestPermission().then(() => {
       listenToMessages();
     }).catch((err) => {
       console.error("取得通知權限失敗:", err);
     });
-
-  } catch (error) {
-    console.error("頁面初始化失敗:", error);
   }
 });
 
